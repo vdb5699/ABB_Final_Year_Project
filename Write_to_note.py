a82234308e9f77ad94fb7c743dafd4073a20c9e3
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-04-19 15:18:39 +1200

    Write file from python
    We need to discuss at what point the methods should be called

---
+++ Write_to_note.py
@@ -0,0 +1,18 @@
+class WriteToNote:
+    def __init__(self, arr):
+        self.content = arr
+        
+    def write(self):
+        for x in range(len(self.content)):
+            if x == 0:
+                f = open("coord_store.txt", w)
+                f.write("x,y")
+            else:
+                f = open("coord_store.txt", a)
+            str =self.content[x][0] + "," + self.content[x][1]
+            f.write(str)
+            f.close()
+
+
+    def set_arr(self, array):
+        self.content = array
