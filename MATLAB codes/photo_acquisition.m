f1bbc16b9a1db6eb5c41a4404e2651cd4bb39384
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-04-22 16:19:01 +1200

    Made a  little change in this class

--- MATLAB codes/photo_acquisition.m
+++ MATLAB codes/photo_acquisition.m
@@ -1,11 +1,17 @@
-camera = webcam()
-preview(camera)
-camera.Resolution = '4416x1242';
+classdef photo_acquisition
+    methods
+        function img = takePhoto(resolution, camID)
+            camera = webcam(camID);
+            camera.Resolution = "4416x1242";
+            preview(camera)
 
-prompt = "Press any key to take photo";
+            prompt = "Press enter key to take photo";
 
-x = input(prompt);
+            input(prompt);
 
-img = snapshot(camera);
-
+            img = snapshot(camera);
+            return
+        end
+    end
+end
 
