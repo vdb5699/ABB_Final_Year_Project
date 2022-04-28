bc0aaaf8ebe04d82233ad8aca880a7c3164e9e30
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-04-28 15:09:01 +1200

    Update main.m

--- MATLAB codes/main.m
+++ MATLAB codes/main.m
@@ -1,21 +1,39 @@
 %  inputImage = imageDatastore('C:\Uni stuff\Project\Model image', 'FileExtensions', [".jfif"]);
- IP = "172.16.44.240";
- port = 34322;
+ IP = "192.168.0.20";
+ port = 1025;
  ph = photo_acquisition();
  res = "2560x720"
+
  im = ph.takePhoto(res,1);
 
- circleDet = circle_detection(im,36);
-% circleDet = circle_detection(im,80);
- cent = circleDet.detectCicle()
+%  circleDet = circle_detection(im,35);
+% % circleDet = circle_detection(im,80);
+%  cent = circleDet.detectCicle()
+% 
+%  colDet = colour_detection(cent,im);
+%  caps = colDet.detectColour()
 
- colDet = colour_detection(cent,im);
- caps = colDet.detectColour()
-
-%  emp = empty_slot_detection(im);
-%  empltySlots = emp.detectSlot(100, 5000);
-
-  sender = send_data(IP, port);
-  [red, brown] = sender.sortCap(caps);
-  sender.send(brown);
-  sender.send(red);
+ emp = empty_slot_detection(im);
+ empltySlots = emp.detectSlot(100, 5000);
+%     imshow(im);
+%   sender = send_data(IP, port);
+%   [brown, red] = sender.sortCap(caps)
+%   a = num2str(height(brown)+height(red))
+%   sender.send(a)
+%   pause(0.5)
+%   for x = 1: height(brown)
+%       prompt = "press enter to send x value"
+%       %input(prompt)
+%       b = num2str(brown(x,1))
+%       sender.send(b);
+%       pause(0.5)
+%       prompt = "press enter to send y value"
+%       %input(prompt)
+%       c = num2str(brown(x,2))
+%       sender.send(c);
+%       pause(0.5)
+%   end
+% 
+%   sender.send(num2str(red(1,1)))
+%   pause(0.5)
+%   sender.send(num2str(red(1,2)))
