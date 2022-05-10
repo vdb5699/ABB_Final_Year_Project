266c7e1a73e987ce51aefcd6eb2e383bdfce5f8f
Anthonykoo <69717197+Anthonykoo@users.noreply.github.com>
2022-05-10 14:30:11 +1200

    Update main.m

--- MATLAB codes/main.m
+++ MATLAB codes/main.m
@@ -3,14 +3,12 @@
  port = 1025;
  ph = photo_acquisition();
  res = "2560x720";
- bP = [1280 720];
- bR = [1227 690];
  flag = 1;
  im = ph.takePhoto(res,1);
 
  %diameter will be changed soon
  circleDet = circle_detection(im,65);
-% circleDet = circle_detection(im,80);
+ %circleDet = circle_detection(im,80);
  cent = circleDet.detectCicle()
 
  colDet = colour_detection(cent,im);
@@ -21,61 +19,84 @@
 %  imshow(im);
   sender = send_data(IP, port);
   tcp = tcpclient(IP,port);
-  [brown, red] = sender.sortCap(caps)
-  a = num2str(height(brown)+height(red))
+  [brown, red] = sender.sortCap(caps);
+  a = num2str(height(brown)+height(red));
   tcp.write(a)
-  
-  prompt = "press enter to send brown cap coordinates"
-  input(prompt)
+  tcp.flush()
+  pause(0.5)
+  disp("sending number of object")
+  tcp.write(num2str(height(brown)));
+  tcp.flush()
+  pause(0.5)
+  tcp.write(num2str(height(red)));
+  tcp.flush()
+  pause(0.5)
   converter = convert_coord();
+
+  disp("Sending brown cap coordinate")
   for x = 1: height(brown)
       flag = 0;
       tcp.write('B')
-      [brown(x,1), brown(x,2)] = converter.convertBrown(brown(x,1),brown(x,2))
-      prompt = "press enter to send x value"
-      input(prompt)
-      b = num2str(brown(x,1))
-      tcp.write(b)
-      pause(0.5)
-      prompt = "press enter to send y value"
-      input(prompt)
-      c = num2str(brown(x,2))
-      tcp.write(c)
       tcp.flush();
-      pause(0.5)
+      pause(3)
+      [brown(x,1), brown(x,2)] = converter.convertBrown(brown(x,1),brown(x,2));
+      xcoord = num2str(brown(x,1))
+      msg = ['sending x value: ',xcoord]
+      disp(msg);
+      tcp.write(a);
+      tcp.flush();
+      pause(3);
+        
+      ycoord = num2str(brown(x,2))
+      msg = ['sending y value: ',ycoord]
+      disp(msg);
+      tcp.write(b);
+      tcp.flush();
+      pause(3);
       
+      disp("waiting for robot to place the bottle");
       while flag == 0
-        test = read(tcp)
+        test = read(tcp);
         if char(test) == "Y"
-            disp(char(test))
+            disp("data received, moving on to next coordinate");
             flag = 1
         end
         pause(0.5)
       end
   end
 
-  prompt = "press enter to send red cap coordinates"
-  input(prompt)
+  disp("Moving on to red bottles")
   
   for y = 1:height(red)
-    flag = 0
+    flag = 0;
     tcp.write('R')
+    tcp.flush()
+    pause(3)
     [red(y,1), red(y,2)] = converter.convertRed(red(y,1),red(y,2))
-    prompt = "press enter to send x value"
-    input(prompt)
-    tcp.write(num2str(red(y,1)))
+
+    xcoord = num2str(red(y,1))
+    msg = ['sending x value: ',xcoord]
+    disp(msg);
+    tcp.write(c)
     tcp.flush();
-    pause(0.5)
-    prompt = "press enter to send y value"
-    input(prompt)
-    tcp.write(num2str(red(y,2)))
+    pause(3)
+
+    ycoord = num2str(red(y,2))
+    msg = ['sending y value: ',ycoord]
+    disp(msg);
+    tcp.write(d)
     tcp.flush();
-    pause(0.5)
+    pause(3)
+
+    disp("waiting for robot to place the bottle");
     while flag == 0
-        test = read(tcp)
-        if char(test) == "Y"
-            disp(char(test))
-            flag = 1
+        test = read(tcp);
+        if char(test) == "Y" && y < height(red)
+            disp("data received, moving on")
+            flag = 1;
+        else
+            disp("That was the last coordinate, ending the program :)")
+            flag = 1;
         end
         pause(0.5)
      end
