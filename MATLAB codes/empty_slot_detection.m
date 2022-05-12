9bb91732df38fbba190b95223324e98804b82f68
ryuoda123 <81938615+ryuoda123@users.noreply.github.com>
2022-05-12 17:21:31 +1200

    Update empty_slot_detection.m

--- MATLAB codes/empty_slot_detection.m
+++ MATLAB codes/empty_slot_detection.m
@@ -4,11 +4,13 @@ classdef empty_slot_detection
         image
         caps
         centroids
+        f
     end
 
     methods
         function obj = empty_slot_detection(im)
             obj.image = im;
+            obj.f = figure("Name","Empty Slots");
         end
 
         function slotList = detectSlot(obj,brightness, areaSize)
@@ -18,7 +20,6 @@ classdef empty_slot_detection
             %white and <= 100 will be black. The value would differ
             %depending on brightness of image.
             bIm = gray < brightness;
-            fig2 = figure('Name','Empty slots')
             imshow(bIm);
             hold on;
             %clear out small areas for better and easier detection
@@ -32,6 +33,7 @@ classdef empty_slot_detection
             centr = [blob.Centroid];
             peri = [blob.Perimeter];
             circul = peri .^2./ (4* pi * ar)
+            brightness
             slotList = eliminateCaps(obj,ar,circul,centr, boundaries);
 %             d = drawline;
 %             pos = d.Position;
@@ -41,6 +43,9 @@ classdef empty_slot_detection
 %             pos = d.Position;
 %             diffPos = diff(pos);
 %             height = hypot(diffPos(1),diffPos(2))
+             if width(slotList) ~= 2 && brightness > 80
+                slotList = detectSlot(obj, brightness-2, areaSize);
+             end
             return
         end
 
@@ -50,17 +55,18 @@ classdef empty_slot_detection
             compList = [sample];
             for objectNumber = 1: length(areas)
                 bound = boundaries{objectNumber};
-
-%                   if ((cir(objectNumber) > 1.5) && (cir(objectNumber) < 3.0)) 
-                    plot(bound(:,2),bound(:,1),'r',LineWidth=3);
+                plot(bound(:,2),bound(:,1),'r',LineWidth=3);
+                   if ((cir(objectNumber) > 1.2) && (cir(objectNumber) < 2.5) && areas(objectNumber) < 130000) 
+                    
                     currentCen = [cen((objectNumber*2)-1) cen(objectNumber*2)]
                     plot(currentCen(1),currentCen(2),'ro','MarkerSize', 30);
                     compList(counter) = EmptySlot([cen((objectNumber*2)-1) cen(objectNumber*2)]) 
                     counter = counter+1;
-% 
-%                  end
-             end
+ 
+                  end
+            end
             return
         end
     end
 end
+
